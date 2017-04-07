9da91eab231b44a126fd40dfa15256b806cbb79c
Bryan Sills <bryan@bryansills.com>
2017-04-06 19:53:08 -0500

    initial commit. almost workininitial commit. almost workingg

---
+++ gradle-json-verify/src/main/java/ninja/bryansills/JsonVerifyPlugin.java
@@ -0,0 +1,10 @@
+package ninja.bryansills;
+
+import org.gradle.api.Project;
+import org.gradle.api.Plugin;
+
+public class JsonVerifyPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project target) {
+    }
+}
