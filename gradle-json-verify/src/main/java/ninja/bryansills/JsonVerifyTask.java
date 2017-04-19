9d29db0fe425b97b70a1cd2dc8ca811bb5e18952
Bryan Sills <bryan@bryansills.com>
2017-04-18 21:47:13 -0500

    json files are now input

--- gradle-json-verify/src/main/java/ninja/bryansills/JsonVerifyTask.java
+++ gradle-json-verify/src/main/java/ninja/bryansills/JsonVerifyTask.java
@@ -1,15 +1,22 @@
 package ninja.bryansills;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.ConfigurableFileTree;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
 
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+
 public class JsonVerifyTask extends DefaultTask {
     @Input
-    public String who = "mate";
+    ConfigurableFileTree configFiles = getProject().fileTree(getProject().getProjectDir() + "/json/");
 
     @TaskAction
     public void jsonVerify() {
-        System.out.println("Hi " + who + "!!!");
+        configFiles.forEach(file -> System.out.println(file.getPath()));
     }
 }
